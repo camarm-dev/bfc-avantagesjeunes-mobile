c02a7b6fba6539228863d0b7aa780f36f446d6c8
camarm <armand@camponovo.xyz>
2023-10-25 20:44:41 +0200

    [Add] commencement de la page de recherche

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-searchbar show-clear-button="always" :animated="true" placeholder="Rechercher un avantage"></ion-searchbar>
+        <ion-searchbar  color="primary" @ionInput="handleInput($event.detail.value)" :value="query" show-clear-button="always" :animated="true" placeholder="Rechercher un avantage"></ion-searchbar>
       </ion-toolbar>
       <ion-toolbar style="height: max-content">
         <ion-list inset class="list-border">
@@ -15,21 +15,24 @@
               <ion-list slot="content">
                 <ion-item>
                   <ion-label>
-                    <p>Rubrique</p>
+                    <p>Rubriques</p>
                   </ion-label>
-                  <ion-select interface="action-sheet" :interface-options="{ color: 'primary' }" placeholder="Toutes" value="all">
-                    <ion-select-option value="all">Toutes</ion-select-option>
-                    <ion-select-option>Cinéma</ion-select-option>
+                  <ion-select @ionChange="rubriques = $event.detail.value" :multiple="true" interface="action-sheet" :interface-options="{ color: 'primary' }" placeholder="Toutes" value="all">
+                    <ion-select-option v-for="rubrique in Object.keys(listeRubriques)" :value="rubrique">{{ listeRubriques[rubrique].nom }}</ion-select-option>
                   </ion-select>
                 </ion-item>
                 <ion-item>
                   <ion-label>
-                    <p>Secteur</p>
+                    <p>Secteurs</p>
                   </ion-label>
-                  <ion-select interface="action-sheet" placeholder="Toutes" value="all">
-                    <ion-select-option value="all">Tous</ion-select-option>
-                    <ion-select-option>Besancon</ion-select-option>
+                  <ion-select @ionChange="secteurs = $event.detail.value" :multiple="true" interface="action-sheet" placeholder="Tous" value="all">
+                    <ion-select-option v-for="secteur in Object.keys(listeSecteurs)" :value="secteur">{{ listeSecteurs[secteur].nom }}</ion-select-option>
                   </ion-select>
+                </ion-item>
+                <ion-item button @click="search()">
+                  <ion-label>
+                    Appliquer
+                  </ion-label>
                 </ion-item>
               </ion-list>
             </ion-accordion>
@@ -38,7 +41,45 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
+      <ion-list v-if="loading" inset>
+        <ion-item class="item-content">
+          <ion-label class="loader">
+            <ion-spinner color="medium" name="crescent"></ion-spinner>
+            <p>Recherche d'avantages...</p>
+          </ion-label>
+        </ion-item>
+      </ion-list>
       <ion-content>
+        <ion-list v-if="!loading && results.length === 0" inset>
+          <ion-item class="item-content">
+            <ion-label class="loader">
+              <Scan class="icon ion-color-medium"/>
+              <p>Aucun avantage trouvé</p>
+            </ion-label>
+          </ion-item>
+        </ion-list>
+        <ion-list v-if="!loading && results.length > 0" inset>
+          <ion-item class="item-content">
+            <ion-label class="loader">
+              <SearchCheck class="icon ion-color-success"/>
+              <p>{{ results_count }} avantages trouvés</p>
+            </ion-label>
+          </ion-item>
+        </ion-list>
+
+        <ion-list>
+          <ion-nav-link :component-props="{ avantage: avantage, favori: isAvantageFavori(avantage.id_avantage) }" router-direction="forward" :component="InspectAvantage" v-for="avantage in results">
+            <div :class="`card focusable ${avantage.type}`">
+              <header>
+                <img alt="Image de l'avantage" :src="avantage.image_url"/>
+              </header>
+              <div class="content">
+                <h3>{{ avantage.offre }}</h3>
+                <p>{{ getInnerContent(avantage.conditions) }}</p>
+              </div>
+            </div>
+          </ion-nav-link>
+        </ion-list>
 
       </ion-content>
     </ion-content>
@@ -56,27 +97,52 @@ import {
   IonAccordion,
   IonAccordionGroup,
   IonItem,
-  IonLabel
+  IonLabel,
+  IonNavLink,
+  IonSelect,
+  IonSelectOption
 } from '@ionic/vue';
 import {
-  Settings2
+  Settings2,
+  Scan,
+  SearchCheck
 } from "lucide-vue-next";
+import { secteurs as listeSecteurs, rubriques as listeRubriques } from "@/functions/interfaces";
+import InspectAvantage from "@/components/InspectAvantage.vue";
 </script>
 
 <script lang="ts">
 import {getAccount} from "@/functions/fetch/account";
-import {getAvantage} from "@/functions/fetch/avantages";
 import {Avantage} from "@/functions/interfaces";
+import {get} from "@/functions/fetch/tools";
 
 export default {
   data () {
     return {
-      avantages: [] as Avantage[],
+      results: [] as Avantage[],
+      results_count: 0,
+      loading: false,
+      query: '',
+      secteurs: [],
+      rubriques: [],
+      favoris: [],
+      searchTimeout: setTimeout(() => {}, 100)
     }
+  },
+  mounted() {
+    getAccount().then(user => {
+      this.favoris = user.favoris
+      if (!this.favoris) this.favoris = []
+    })
   },
   methods: {
     open(url: string) {
       window.open(url)
+    },
+    handleInput(query: string) {
+      clearTimeout(this.searchTimeout)
+      this.query = query
+      this.searchTimeout = setTimeout(this.search, 1500)
     },
     goTo(href: string) {
       this.$router.push(href)
@@ -85,7 +151,30 @@ export default {
       const el = document.createElement('div')
       el.innerHTML = html_string
       return el.innerText
-    }
+    },
+    isAvantageFavori(id_avantage: string) {
+      return this.favoris.includes(id_avantage)
+    },
+    async search() {
+      this.loading = true
+      console.log(this.secteurs)
+      console.log(this.rubriques)
+      let url = `${localStorage.getItem('userApiUrl')}/search?q=${this.query}`
+
+      if (this.secteurs.length > 0) {
+        url += `&secteurs=${this.secteurs.join(',')}`
+      }
+      if (this.rubriques.length > 0) {
+        url += `&rubriques=${this.rubriques.join(',')}`
+      }
+
+      const response = await get(url) as any
+
+      this.results = response.results
+      this.results_count = response.count
+
+      this.loading = false
+    },
   },
 }
 </script>
@@ -108,13 +197,53 @@ ion-item-divider {
 
 ion-toolbar .sc-ion-searchbar-ios-h {
   padding-top: 15px;
-  --background: var(--ion-color-tertiary);
-  --color: var(--ion-color-tertiary-contrast);
+  --background: var(--ion-color-primary);
+  --color: var(--ion-color-primary-contrast);
   color: var(--color) !important;
 }
 
-ion-toolbar .sc-ion-searchbar-ios-h.ion-color-tertiary * {
-  --color: var(--ion-color-tertiary-contrast);
+ion-toolbar .sc-ion-searchbar-ios-h.ion-color-primary * {
+  --color: var(--ion-color-primary-contrast);
   color: var(--color) !important;
 }
+
+ion-alert {
+  --background: var(--ion-color-primary) !important;
+}
+
+ion-alert .alert-button {
+  color: #fff !important;
+}
+
+ion-alert [aria-checked="true"].sc-ion-alert-ios .alert-checkbox-icon.sc-ion-alert-ios {
+  border-color: var(--ion-color-secondary);
+  background-color: var(--ion-color-secondary);
+}
+
+.item-content::part(native) {
+  display: flex;
+  flex-direction: column;
+}
+
+.loader {
+  display: flex !important;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 5px;
+  margin-right: 0;
+}
+
+.loader ion-spinner {
+  margin-bottom: 5px;
+  margin-inline-end: 0;
+}
+
+.loader .icon {
+  margin-bottom: 5px;
+  margin-inline-end: 0;
+  height: 30px;
+  width: 30px;
+}
+
 </style>
