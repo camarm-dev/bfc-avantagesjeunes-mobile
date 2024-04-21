952457e7547d50fc630d19aa55ae4eb4ad74b3da
camarm-dev <contact@camarm.dev>
2024-04-21 19:14:27 +0200

    feat: added a pulse animation on news card

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -2,26 +2,29 @@
 
 import {IonNavLink, IonSkeletonText} from "@ionic/vue";
 import ReadNews from "@/components/ReadNews.vue";
+import PulseItem from "@/components/PulseItem.vue"
 </script>
 
 <template>
-  <ion-nav-link :component-props="{ article: article }" router-direction="forward" :component="ReadNews">
-    <div class="card focusable news full">
-      <header>
-        <img v-if="article.image_url" alt="Image de l'avantage" :src="article.image_url"/>
-        <ion-skeleton-text class="image" v-else :animated="true"></ion-skeleton-text>
-      </header>
-      <div class="content" v-if="!loading">
-        <h3>{{ article.titre }}</h3>
-        <p>{{ getInnerContent(article.contenu) }}</p>
+  <pulse-item>
+    <ion-nav-link :component-props="{ article: article }" router-direction="forward" :component="ReadNews">
+      <div class="card focusable news full">
+        <header>
+          <img v-if="article.image_url" alt="Image de l'avantage" :src="article.image_url"/>
+          <ion-skeleton-text class="image" v-else :animated="true"></ion-skeleton-text>
+        </header>
+        <div class="content" v-if="!loading">
+          <h3>{{ article.titre }}</h3>
+          <p>{{ getInnerContent(article.contenu) }}</p>
+        </div>
+        <div class="content" v-else>
+          <ion-skeleton-text :animated="true" style="width: 80%; margin-top: 2em;"></ion-skeleton-text>
+          <ion-skeleton-text :animated="true" style="width: 100%;"></ion-skeleton-text>
+          <ion-skeleton-text :animated="true" style="width: 20%;"></ion-skeleton-text>
+        </div>
       </div>
-      <div class="content" v-else>
-        <ion-skeleton-text :animated="true" style="width: 80%; margin-top: 2em;"></ion-skeleton-text>
-        <ion-skeleton-text :animated="true" style="width: 100%;"></ion-skeleton-text>
-        <ion-skeleton-text :animated="true" style="width: 20%;"></ion-skeleton-text>
-      </div>
-    </div>
-  </ion-nav-link>
+    </ion-nav-link>
+  </pulse-item>
 </template>
 
 <script lang="ts">
