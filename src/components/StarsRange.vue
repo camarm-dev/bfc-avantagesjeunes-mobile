4da7425ee7f0b48d5f88bdcc02c79a0aa853fd3f
camarm-dev <contact@camarm.dev>
2024-05-05 10:22:02 +0200

    add: star range POC

---
+++ src/components/StarsRange.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {
+  Star
+} from 'lucide-vue-next'
+</script>
+<template>
+  <div class="star-range">
+    <Star v-if="editable" @click="setNote(i + 1)" :class="`star ${i + 1 <= dynamicNote ? 'filled': ''}`" v-for="i in Array(5).keys()"/>
+    <Star v-else :class="`star ${i + 1 <= note ? 'filled': ''}`" v-for="i in Array(5).keys()"/>
+    <p v-if="label">{{ dynamicNote }} / 5</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  props: [
+      "editable",
+      "note",
+      "label"
+  ],
+  data() {
+    return {
+      dynamicNote: this.note
+    }
+  },
+  methods: {
+    setNote(note: number) {
+      this.dynamicNote = note
+    }
+  }
+}
+</script>
+
+<style scoped>
+.star-range {
+  width: max-content;
+}
+
+.star {
+  margin-left: .2em;
+  margin-right: .2em;
+  color: var(--ion-color-star);
+}
+
+.star.filled {
+  fill: var(--ion-color-star)
+}
+</style>
