1a4f57f90d89c7942b23e9e5725f135ec66bb3bb
camarm-dev <contact@camarm.dev>
2024-05-19 16:04:41 +0200

    fix: mise à jour des liens vers les documents légaux

--- src/components/LoginModal.vue
+++ src/components/LoginModal.vue
@@ -32,7 +32,7 @@
           <ion-item class="ion-border">
             <ion-checkbox color="secondary" justify="start" :checked="agree" @ionChange="changeAgreeStatus($event.detail.checked)" label-placement="end" required>
               <ion-label class="ion-text-wrap" color="light">
-                J'accepte les <a href="https://github.com/camarm-dev/bfc-avantagesjeunes-mobile/blob/main/CGU.md" target="_blank">conditions d'utilisation</a> de Avantages Jeunes Connect et j'ai pris connaissance du
+                J'accepte les <a href="https://avantagesjeunesconnect.camarm.fr/cgu" target="_blank">conditions générales d'utilisation</a> ainsi que <a href="https://avantagesjeunesconnect.camarm.fr/privacy" target="_blank">la politique de confidentialité</a> de Avantages Jeunes Connect et j'ai pris connaissance du
                 <a href="https://github.com/camarm-dev/bfc-avantagesjeunes-mobile">code</a> et de <a href="https://github.com/camarm-dev/bfc-avantagesjeunes-mobile/blob/main/LICENSE">sa license</a>.
               </ion-label>
             </ion-checkbox>
