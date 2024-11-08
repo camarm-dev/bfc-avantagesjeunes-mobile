6e3a0000082e5ee3a8ea47da9015b12da21817ff
camarm-dev <contact@camarm.dev>
2024-11-08 23:49:22 +0100

    fix: typing of response /payloads

--- src/types/account.ts
+++ src/types/account.ts
@@ -45,6 +45,90 @@ export type Account = {
     suggestions: SuggestedAvantage[]
 }
 
+export type CheckConnectionCodeInfosResponse = {
+    code: {
+        code: string,
+        numero: string,
+        rid_pack: 1,
+        actif: false,
+        actif_datetime: null,
+        saison: string
+    },
+    carte: {
+        id_carte: number,
+        date_vente: string,
+        datetime_update: string,
+        nom: string,
+        prenom: string,
+        adresse: string,
+        cp: string,
+        ville: string,
+        tel: string,
+        tel_accepte: null,
+        email: string,
+        email_confirm: null,
+        email_accepte: null,
+        statut: number,
+        numero: string,
+        date_naiss: string,
+        remarque: null,
+        rid_tarif: number,
+        valid: boolean,
+        rid_admin_user: number,
+        rid_pack: number,
+        sexe: string,
+        mode_paiement: number,
+        rid_type: null,
+        type_inscription: null,
+        rid_compte: null,
+        saison: string,
+        image_carte_identite: string,
+        paiement: number,
+        date_paie: null,
+        total: string,
+        frais_port: string,
+        avoir: string,
+        avoir_code: string,
+        rid_avoir: number,
+        livraison: number,
+        id_point_relais: number,
+        livret: boolean,
+        rid_commande: number,
+        carte_retiree: boolean,
+        carte_retiree_datetime: string,
+        contact_banque_pop: boolean,
+        valeur: null,
+        monnaie: null,
+        type: null,
+        age: null,
+        admin_utilisateur: null,
+        point_relais: null
+    },
+    status: boolean,
+    status_code: number
+}
+
+export type FinishSigningData = {
+    nom: string,
+    prenom: string,
+    adresse: string,
+    cp: string,
+    ville: string,
+    tel: string,
+    tel_accepte: boolean,
+    email_accepte: boolean,
+    date_naiss: string,
+    emailNew: string,
+    emailConfirm: string,
+    newsletter: string,
+    sexe: "h" | "f" | string,
+    statut: number,
+    numero: string,
+    id_carte: number,
+    photo: string,
+    email: string
+}
+
 export type UserCard = {
     id_carte: number
     date_vente: string
