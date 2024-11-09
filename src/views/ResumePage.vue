e1541622bb0ce111f844894f474a5a493fda7eaf
camarm-dev <contact@camarm.dev>
2024-11-09 13:15:05 +0100

    feat: error disclaimer

--- src/views/ResumePage.vue
+++ src/views/ResumePage.vue
@@ -53,6 +53,15 @@
         </div>
         <AvantageCard :key="favori.id_avantage" :used="usedAdvantagesIds.includes(favori.id_avantage)" :favori="true" :avantage="favori" v-for="favori in user.favoris"/>
       </div>
+      <pulse-item vibrate>
+        <ion-list inset v-if="!loggedIn">
+          <ion-item @click="refreshAccount()">
+            <ion-note class="ion-padding" color="light">
+              Mmmh 🤔 Il semblerait que nous avons un problème pour charger les dernières informations. Clique ici pour recharger. Déconnecte toi si cela se reproduit.
+            </ion-note>
+          </ion-item>
+        </ion-list>
+      </pulse-item>
       <ion-nav-link router-direction="forward" :component="InspectProfile" :component-props="{ editable: true, id: user.id_compte }">
         <pulse-item vibrate>
           <ion-list inset v-if="firstConnectionDisclaimer">
